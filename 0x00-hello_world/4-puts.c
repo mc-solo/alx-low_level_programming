b67e6bd1751f3d74adf8b168f885d660e2862c25
Wondwosen <revatoel@gmail.com>
2023-06-15 14:08:45 -0700

    First C program

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -4,9 +4,9 @@
  *
  * Return: Always 0 (Success)
  */
-int main (void)
+int main(void)
 {
-	puts("\"Programming is like building a multilingual puzzle\n);
+	puts("\"Programming is like building a multilingual puzzle\n");
 	return (0);
 
 }
