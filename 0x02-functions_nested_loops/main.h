1c29133ccfe9adbdd7a7c45d2d1ed773c22805d7
Wondwosen <revatoel@gmail.com>
2023-06-21 18:53:58 -0700

    add files

---
+++ 0x02-functions_nested_loops/main.h
@@ -0,0 +1,14 @@
+#ifndef main_h
+#define main_h
+
+int _putchar(char);
+void print_alphabet(void);
+int _islower(int c);
+int _isalpha(int c);
+int print_last_digit(int);
+void jack_bauer(void);
+void times_table(void);
+int add(int, int);
+void print_times_table(int n);
+
+#endif
