c788a043394ac150e59314b360a3933750d85e2d
Wondwosen <revatoel@gmail.com>
2023-06-26 11:50:33 -0700

    reset

---
+++ 0x05-pointers_arrays_strings/0-reset_to_98.c
@@ -0,0 +1,13 @@
+#include "main.h"
+
+/**
+ * reset_to_98 - a fx that takes a pointer as it's argument and updates
+ * the value it ponts to
+ * @n: input
+ * retuen : n
+ */
+void reset_to_98(int *n)
+{
+	*n = 98;
+}
+
