8c93d7f48a61ec8e7a29508644510240669a6f2d
Wondwosen <revatoel@gmail.com>
2023-07-04 13:15:13 -0700

    add some tasks

---
+++ 0x07-pointers_arrays_strings/5-rev_string.c
@@ -0,0 +1,37 @@
+#include "main.h"
+
+
+/**
+ * rev_string - prints a string in reverse
+ * @s: pointer to the string to be reversed
+ *
+ * Return: void
+ */
+
+void rev_string(char *s)
+{ 
+	int i = 0;
+	int j;
+	int right;
+
+	while (s[i])
+	{
+		i++;
+	}
+
+	j = i / 2;
+	right = 0;
+
+	while (right != j)
+	{
+		char temp = s[right];
+		int left = i - right - 1;
+
+		s[right] = s[left];
+		s[left] = s[temp];
+
+		right++;
+
+	}
+	
+}
