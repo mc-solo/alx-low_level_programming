423431fb65c4e7dead6f61e62dad76bd6c5a4667
Wondwosen <revatoel@gmail.com>
2023-07-05 13:07:59 -0700

    edit puts

--- 0x08-recursion/0-puts_recursion.c
+++ 0x08-recursion/0-puts_recursion.c
@@ -1,14 +1,14 @@
 #include "main.h"
 /**
- * *_puts_recursion - print a string to the cosole
+ * _puts_recursion - print a string to the cosole
  * @s: pointer to a string to print
- * return: void 
+ * return : void 
  */
 
 void _puts_recursion(char *s)
 {
-	if(*s == '\0')
-	 	_putchar('\n');
+	if (*s == '\0')
+		_putchar('\n');
 	else
 	{
 		_putchar(s[0]);
