9a2fc70d4bb536d7c28e3792eebc6652104bc77e
Wondwosen <revatoel@gmail.com>
2023-07-05 13:58:06 -0700

    add factorial recursion

---
+++ 0x08-recursion/3-factorial.c
@@ -0,0 +1,24 @@
+#include "main.h"
+
+/**
+ * factorial - gives the factorial of a number
+ * @n: input number
+ * retun: factorial for n >= 0 & -1 if n <0 
+ */
+int factorial(int n)
+{
+	int result = n;
+
+	if (n < 0)
+	{
+		return (-1)
+	
+	}
+	else if (n >= 0 && n <= 1)
+		return (1);
+
+	result *= factorial(n - 1);
+
+	return (result);
+}
+
