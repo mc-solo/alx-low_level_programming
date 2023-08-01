c8882da2200881d1bcb7d5692ed76f4d99c2548d
Wondwosen <revatoel@gmail.com>
2023-08-01 12:31:52 -0700

    add README.md and prints list

---
+++ 0x12-singly_linked_lists/0-prints_list.c
@@ -0,0 +1,28 @@
+#include "lists.h"
+#include <stdlib.h>
+#include <stdio.h>
+/**
+ * print_list - Prints all the elements of a list_t list.
+ * @h: The list_t list.
+ *
+ * Return: The number of nodes in the linked list
+ */
+
+size_t print_list(const list_t *h)
+{
+	size_t node = 0;
+
+	while (h)
+	{
+		if (h->str == NULL)
+			printf("[0] (nil)\n");
+
+		else
+			printf("[%d] %s\n", h->len, h->str);
+
+		node++;
+		h = h->next;
+	}
+
+	return (node);
+}
---
+++ 0x12-singly_linked_lists/README.md
@@ -0,0 +1,3 @@
+#Introduction to singly linked lists in c.
+
+The first task on singly linked lists.
