b5eccf6e07824b2defca5fe336ae297ad3f19f0a
Wondwosen <revatoel@gmail.com>
2023-08-01 12:35:32 -0700

    fix print list

--- 0x12-singly_linked_lists/0-print_list.c
+++ 0x12-singly_linked_lists/0-print_list.c
@@ -5,7 +5,7 @@
  * print_list - Prints all the elements of a list_t list.
  * @h: The list_t list.
  *
- * Return: The number of nodes in the linked list
+ * Return: The number of nodes in the linked list.
  */
 
 size_t print_list(const list_t *h)
