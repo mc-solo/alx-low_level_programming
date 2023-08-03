5c951e02d6773330b1731487b5e789c6a4e7eccd
Wondwosen <revatoel@gmail.com>
2023-08-03 16:26:14 -0700

    clear trailing space

--- 0x14-bit_manipulation/3-set_bit.c
+++ 0x14-bit_manipulation/3-set_bit.c
@@ -1,16 +1,19 @@
 #include "main.h"
-#include <stdlib.h>
 
 /**
- * set_bit - sets the value of a bit to 1 at a given index
- * @n: the value whose bit is to be set to 1
- * @index: position of the bit 
- * Return: 1 if it worked, -1 if error occured
- */
+*set_bit - sets a bit at position to 1
+*@n: number to be used
+*@index: index to be set
+*
+*Return: 1 on success, -1 on failure
+*/
+
 int set_bit(unsigned long int *n, unsigned int index)
 {
-	if (index > sizeof(n) * 8)
+	if (n == NULL || (index > (sizeof(unsigned long int) * 8) - 1))
 		return (-1);
+
 	*n |= (1 << index);
 	return (1);
 }
+
